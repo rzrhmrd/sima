e0762bc73fa51005615b2a4ee974e629a8a27109
rzmmzdh <rzmmzdh@gmail.com>
2023-01-16 16:53:30 +0330

    request read phone state permission

--- app/src/main/java/com/rzmmzdh/sima/MainActivity.kt
+++ app/src/main/java/com/rzmmzdh/sima/MainActivity.kt
@@ -1,33 +1,64 @@
 package com.rzmmzdh.sima
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rzmmzdh.sima.ui.theme.SimaTheme
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.work.*
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.rzmmzdh.sima.feature_sim.core.theme.SimaTheme
+import com.rzmmzdh.sima.feature_sim.ui.SimScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@OptIn(ExperimentalPermissionsApi::class)
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            val permissionState =
+                rememberPermissionState(
+                    android.Manifest.permission.READ_PHONE_STATE,
+                )
+
             SimaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LaunchedEffect(key1 = permissionState) {
+                        if (!permissionState.status.isGranted) {
+                            permissionState.launchPermissionRequest()
+                        }
+                    }
+
+
+                    NavHost(navController = navController, startDestination = "SIM_SCREEN") {
+                        composable(route = "SIM_SCREEN") {
+                            SimScreen(phonePermissionState = permissionState)
+                        }
+                    }
                 }
+                HideSystemUi()
             }
         }
     }
-}
 
 @Composable
 fun Greeting(name: String) {
