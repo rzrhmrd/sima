6182f1a7e7fbe8b0e6e57867e9fb48b6bf6d844a
rzmmzdh <rzmmzdh@gmail.com>
2023-01-17 21:16:09 +0330

    add network type resolve #9

--- app/src/main/java/com/rzmmzdh/sima/feature_sim/ui/SimScreen.kt
+++ app/src/main/java/com/rzmmzdh/sima/feature_sim/ui/SimScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.foundation.layout.Arrangement.SpaceEvenly
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CutCornerShape
@@ -22,14 +23,12 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDirection
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -154,11 +153,17 @@ private fun Sims(
                         .fillMaxSize()
                         .padding(8.dp),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceAround
+                    horizontalArrangement = Center
                 ) {
                     sim.let {
                         SignalStrength(it.signalStrength)
-                        Carrier(it.carrierName)
+                        Column(
+                            verticalArrangement = SpaceEvenly,
+                            horizontalAlignment = CenterHorizontally
+                        ) {
+                            Carrier(it.carrierName)
+                            NetworkType(networkTypeName(it.networkType))
+                        }
                         SimSlot(number = it.slotNumber, isTopSim = it == topSim)
                     }
                 }
@@ -171,6 +176,34 @@ private fun Sims(
         }
     }
 }
+
+@Composable
+fun NetworkType(networkType: String) {
+    Text(networkType)
+}
+
+fun networkTypeName(type: Int) =
+    when (type) {
+        TelephonyManager.NETWORK_TYPE_1xRTT -> "1xRTT"
+        TelephonyManager.NETWORK_TYPE_CDMA -> "CDMA"
+        TelephonyManager.NETWORK_TYPE_EDGE -> "EDGE"
+        TelephonyManager.NETWORK_TYPE_EHRPD -> "eHRPD"
+        TelephonyManager.NETWORK_TYPE_EVDO_0 -> "EVDO rev. 0"
+        TelephonyManager.NETWORK_TYPE_EVDO_A -> "EVDO rev. A"
+        TelephonyManager.NETWORK_TYPE_EVDO_B -> "EVDO rev. B"
+        TelephonyManager.NETWORK_TYPE_GPRS -> "GPRS"
+        TelephonyManager.NETWORK_TYPE_HSDPA -> "HSDPA"
+        TelephonyManager.NETWORK_TYPE_HSPA -> "HSPA"
+        TelephonyManager.NETWORK_TYPE_HSPAP -> "HSPA+"
+        TelephonyManager.NETWORK_TYPE_HSUPA -> "HSUPA"
+        TelephonyManager.NETWORK_TYPE_IDEN -> "iDen"
+        TelephonyManager.NETWORK_TYPE_LTE -> "LTE"
+        TelephonyManager.NETWORK_TYPE_UMTS -> "UMTS"
+        TelephonyManager.NETWORK_TYPE_UNKNOWN -> "Unknown"
+        else -> {
+            "Unkown"
+        }
+    }
 
 @Composable
 private fun SimSlot(
@@ -299,6 +332,7 @@ private fun simStatus(
                     subId = sub.subscriptionId,
                     slotNumber = sub.simSlotIndex + 1,
                     carrierName = sub.carrierName.toString(),
+                    networkType = telephonyManager.createForSubscriptionId(sub.subscriptionId).dataNetworkType,
                     signalStrength = telephonyManager.createForSubscriptionId
                         (sub.subscriptionId).signalStrength?.cellSignalStrengths?.firstNotNullOf {
                         it.dbm
